831823849e6dcd55274d15645caeb61bb6ecab06
Angel Pizarro <delagoya@gmail.com>
2018-06-25 17:32:43 -0400

    added section to parse ENV for AWS creds

--- samtools-wrapper.sh
+++ samtools-wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 B='http://169.254.169.254/latest/meta-data/iam/security-credentials/'
 CREDS=''
-if [ -n "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]; then
+if [ -n "${AWS_ACCESS_KEY_ID}" ]; then
+    CREDS="AWS_SECRET_ACCESS_KEY='${AWS_SECRET_ACCESS_KEY}' AWS_ACCESS_KEY_ID='${AWS_ACCESS_KEY_ID}'"
+elif [ -n "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]; then
     CREDS=$(curl -s --connect-timeout 0.1 -s 169.254.170.2${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI} |  jq -r "\"AWS_ACCESS_KEY_ID='\(.AccessKeyId)' AWS_SECRET_ACCESS_KEY='\(.SecretAccessKey)' AWS_SESSION_TOKEN='\(.Token)'\"")
 elif [ -n "$(curl -s --connect-timeout 0.1 $B)" ]; then
     R=$(curl -s --connect-timeout 0.1 $B)
